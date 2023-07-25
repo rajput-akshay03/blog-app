d850e62cf6fb0278aaf5da693681fceb98976eb4
rajput-akshay03 <akshayrajput152003@gmail.com>
2023-07-25 19:40:29 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 
+import { useContext } from 'react';
 import './App.css';
+import { appcontext } from './context/appContext';
 
 function App() {
+  const {loading} = useContext(appcontext);
   return (
     <div className="App">
-         
+         <header className='header'>Tech Blogs</header>
+         <div>
+          {
+            loading?(<Spinner/>):()
+          }
+         </div>
     </div>
   );
 }
